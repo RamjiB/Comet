e1aa78c2993d0b9ec29a17cb0d503c85e4844e6d
RamjiB <ramji.b28@gmail.com>
2019-09-13 22:34:19 -0400

    code changed with validation split

--- Vgg_model.py
+++ Vgg_model.py
@@ -1,3 +1,4 @@
+import glob
 import keras
 from keras.applications import inception_v3,nasnet,mobilenet,vgg19,resnet50,xception,densenet
 import keras.backend as K
@@ -11,29 +12,40 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint,ReduceLROnPlateau,CSVLogger
 from keras.optimizers import Adam,RMSprop
 
+total_train_images = len(glob.glob('train/damaged/*')) + len(glob.glob('train/undamaged/*'))
+total_valid_images = len(glob.glob('valid/damaged/*')) + len(glob.glob('valid/undamaged/*'))
+IMG_SIZE = 128
+BATCH_SIZE = 32
+EPOCHS = 100
+
 datagen = ImageDataGenerator(rescale=1.0/255,
                 horizontal_flip=True,
                 vertical_flip=True)
 
-#resizing all the images to 96 x 96
+#resizing all the images to 128 x 128
 train_gen = datagen.flow_from_directory('train/' , 
-                                        target_size = (128,128) , 
-                                        batch_size = 32,
+                                        target_size = (IMG_SIZE,IMG_SIZE) , 
+                                        batch_size = BATCH_SIZE,
+                                       class_mode ='binary',
+                                       shuffle = True)
+valid_gen = datagen.flow_from_directory('valid/' , 
+                                        target_size =(IMG_SIZE,IMG_SIZE) , 
+                                        batch_size = BATCH_SIZE,
                                        class_mode ='binary',
                                        shuffle = True)
 def pretrained_model(model):
     if model == 'densenet':
-        base_model = densenet.DenseNet121(include_top=False,weights='imagenet',input_shape = (128,128,3))
+        base_model = densenet.DenseNet121(include_top=False,weights='imagenet',input_shape = (IMG_SIZE,IMG_SIZE,3))
     elif model == 'inception':
-        base_model = inception_v3.InceptionV3(include_top=False,weights='imagenet',input_shape = (128,128,3))
+        base_model = inception_v3.InceptionV3(include_top=False,weights='imagenet',input_shape = (IMG_SIZE,IMG_SIZE,3))
     elif model == 'mobilenet':
-        base_model = mobilenet.MobileNet(include_top=False,weights='imagenet',input_shape = (128,128,3))
+        base_model = mobilenet.MobileNet(include_top=False,weights='imagenet',input_shape = (IMG_SIZE,IMG_SIZE,3))
     elif model == 'vgg':
-        base_model = vgg19.VGG19(include_top=False,weights='imagenet',input_shape = (128,128,3))
+        base_model = vgg19.VGG19(include_top=False,weights='imagenet',input_shape = (IMG_SIZE,IMG_SIZE,3))
     elif model == 'resnet':
-        base_model = resnet50.ResNet50(include_top=False,weights='imagenet',input_shape = (128,128,3))
+        base_model = resnet50.ResNet50(include_top=False,weights='imagenet',input_shape = (IMG_SIZE,IMG_SIZE,3))
     elif model == 'xception':
-        base_model = xception.Xception(include_top=False,weights='imagenet',input_shape = (128,128,3))
+        base_model = xception.Xception(include_top=False,weights='imagenet',input_shape = (IMG_SIZE,IMG_SIZE,3))
         
     for layer in base_model.layers:
         layer.trainable = True
@@ -52,7 +64,7 @@ main_model.summary()
 csv_logger = CSVLogger("vgg_model.csv",separator = ",",append=True)
 
 checkpoint_fp = "vgg_model_best.h5"
-checkpoint = ModelCheckpoint(checkpoint_fp,monitor='acc',
+checkpoint = ModelCheckpoint(checkpoint_fp,monitor='val_acc',
                              verbose=1,
                             save_best_only= True,mode='max')
 
@@ -65,13 +77,16 @@ learning_rate = ReduceLROnPlateau(monitor='acc',
 
 callback = [checkpoint,learning_rate,csv_logger]
 
-steps_p_ep_tr = 1013//32
+steps_p_ep_tr = total_train_images//BATCH_SIZE
+steps_p_ep_va = total_valid_images//BATCH_SIZE
 
 main_model.compile(optimizer = Adam(lr=0.0001), 
               loss = 'binary_crossentropy', metrics=['accuracy'])
 
 my_model = main_model.fit_generator(train_gen,
                                    steps_per_epoch = steps_p_ep_tr,
+				   validation_data = valid_gen,
+                                   validation_steps = steps_p_ep_va,
                                    verbose = 1,
                                    epochs = 100,
                                    callbacks = callback)
